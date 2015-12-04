f6367d8ccb0fa1688fda08c1c01fca04102cff85
Thom Householder <thomhouseholder@gmail.com>
2015-12-04 15:05:22 -0500

    added candidates to array

---
+++ models/candidate.js
@@ -0,0 +1,22 @@
+var candidates = [
+  ["Hillary Clinton", "D"],
+  ["Bernie Sanders", "D"],
+  ["Martin O'Malley", "D"],
+  ["Donald Trump", "R"],
+  ["Ben Carson", "R"],
+  ["Jeb Bush", "R"],
+  ["Marco Rubio", "R"],
+  ["Carly Fiorina", "R"],
+  ["Ted Cruz", "R"],
+  ["Rand Paul", "R"],
+  ["Chris Christie", "R"]
+];
+
+function Candidate(){
+};
+
+Candidate.all = function(){
+  return candidates;
+};
+
+module.exports = Candidate
