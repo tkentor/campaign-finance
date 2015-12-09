b2dadee46475b475115a060996017e65b67f32b8
Teddy Kentor <tkentor@gmail.com>
2015-12-09 11:56:13 -0500

    third commit, no d3 yet

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,10 +2,33 @@ $(document).ready(function() {
 
   $(function() {
       $(".financials").hide();
+      $("#flag").hide();
+      $(".animals").hide();
+      $("#flag-2").hide();
   });
 
   $(".faces").click(function() {
     $(".financials").show("slow");
   });
+
+  $("#button-1").click(function() {
+    $(".animals").hide();
+    $("#flag-2").hide();
+    $("#flag").show("slow");
+  });
+
+  $("#button-2").click(function() {
+    $("#flag").hide();
+    $("#flag-2").hide();
+    $(".animals").show("slow");
+  });
+
+  $("#button-3").click(function() {
+    $("#flag").hide();
+    $(".animals").hide();
+    $("#flag-2").show("slow");
+  });
+
+
 
 });
